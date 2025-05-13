fc8b0f757fd86e26998ba04b713fd26b7f0543a4
Gautier Ben Aïm <benaim.gautier@gmail.com>
2025-05-13 18:21:20 +0200

    feat: basic form with comment

--- src/components/FeedbackWidget/Wigdet.client.tsx
+++ src/components/FeedbackWidget/Wigdet.client.tsx
@@ -1,15 +1,14 @@
 import { useCallback, useState } from "react";
 import classes from "./component.module.css";
+import { useTranslation as useT } from "react-i18next"; // Make the vscode extension happy
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
 declare namespace wem {
-  type Properties = Record<string, unknown>;
-
   interface Item {
     scope: string;
     itemId: string;
     itemType: string;
-    properties: Properties;
+    properties: Record<string, unknown>;
   }
 
   interface Event {
@@ -19,17 +18,13 @@ declare namespace wem {
     target: Item;
   }
 
-  interface Events {
-    events: Event[];
-  }
-
-  function buildSource(id: string, type: string, properties?: Properties): Item;
-  function buildTarget(id: string, type: string, properties?: Properties): Item;
+  function buildSource(id: string, type: string, properties?: Record<string, unknown>): Item;
+  function buildTarget(id: string, type: string, properties?: Record<string, unknown>): Item;
   function buildSourcePage(): Item;
   function buildTargetPage(): Item;
   function buildEvent(type: string, target: Item, source: Item): Event;
   function collectEvents(
-    events: Events,
+    events: { events: Event[] },
     successCallback?: () => void,
     errorCallback?: () => void,
   ): void;
@@ -37,37 +32,71 @@ declare namespace wem {
 
 export default function Widget() {
   const [happy, setHappy] = useState<boolean>();
+  const [state, setState] = useState<"sending" | "sent">();
 
-  const handler = (value: boolean) =>
+  const { t } = useT("feedback-widget");
+
+  const handler = (happy: boolean) =>
     useCallback(() => {
-      if (happy === value) return;
       const source = wem.buildSourcePage();
-      const target = wem.buildTarget("click", "feedback", { happy: value });
-      const newEvent = wem.buildEvent("click", target, source);
-      wem.collectEvents({ events: [newEvent] });
-      setHappy(value);
-    }, [happy]);
+      const target = wem.buildTarget("click", "feedback", { happy });
+      wem.collectEvents({ events: [wem.buildEvent("click", target, source)] });
+      setHappy(happy);
+    }, []);
 
   return (
-    <aside>
-      <h3>Was this helpful?</h3>
-      <button type="button" onClick={handler(true)}>
-        Yes
-      </button>
-      <button type="button" onClick={handler(false)}>
-        No
-      </button>
+    <aside className={classes.card}>
+      <div inert={happy !== undefined} className={classes.buttons}>
+        {t("e8YBCRcMqxPeIzrSCE2SZ")}
+        <button type="button" className={classes.input} onClick={handler(true)}>
+          {t("sXVG2hExuwfFlejOPijT0")}
+        </button>
+        <button type="button" className={classes.input} onClick={handler(false)}>
+          {t("YRkfbP0aWFxjLZZyV5V8Q")}
+        </button>
+      </div>
       {happy !== undefined && (
-        <div className={classes.feedback}>
-          {happy ? (
-            <p className={classes.happy}>Glad to hear that!</p>
-          ) : (
-            <p className={classes.sad}>Sorry to hear that!</p>
-          )}
-          <button type="button" className={classes.close} onClick={() => setHappy(undefined)}>
-            Close
-          </button>
-        </div>
+        <form
+          className={classes.form}
+          onSubmit={(event) => {
+            if (state !== undefined) return;
+
+            event.preventDefault();
+            const data = new FormData(event.currentTarget);
+            const comment = data.get("comment");
+
+            if (!comment) {
+              setState("sent");
+              return;
+            }
+
+            const source = wem.buildSourcePage();
+            const target = wem.buildTarget("comment", "feedback", { happy, comment });
+            setState("sending");
+            wem.collectEvents({ events: [wem.buildEvent("click", target, source)] }, () => {
+              setState("sent");
+            });
+          }}
+        >
+          <label className={classes.label}>
+            {happy ? t("fnJQRxmjVi1lk7KCH9CMW") : t("Mp-3D66q_s9pYf6vEvSi_")}
+            <textarea
+              name="comment"
+              readOnly={state !== undefined}
+              className={classes.input}
+              style={{ width: "100%" }}
+            />
+          </label>
+          <div className={classes.submit}>
+            <button type="submit" className={classes.input} disabled={state !== undefined}>
+              {t("zS9_INlrschFjnnywiIrI")}
+            </button>
+            {state === "sending" && (
+              <span aria-label={t("WJL_PlnllYZoK4DY72LCA")} className={classes.spinner}></span>
+            )}
+            {state === "sent" && <span>{t("WvOu13wykUW2XlplY4nwZ")}</span>}
+          </div>
+        </form>
       )}
     </aside>
   );
